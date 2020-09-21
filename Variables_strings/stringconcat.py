7aa661e943035e15eaaf2bb6949ddc45159a414c
Sean Granier <seanlg624@gmail.com>
2020-09-21 16:03:28 -0500

    update practice

--- Variables_strings/stringconcat.py
+++ Variables_strings/stringconcat.py
@@ -1,5 +1,11 @@
-greeting = "Yo whats up"
+'''greeting = "Yo whats up"
 name = "bru?"
 greet_name = greeting + " " +name
 
-print(greet_name)
+print(greet_name)'''
+
+first = "Sean"
+last = "Granier"
+
+formatted = f"My fist name is {first} and then my last name is {last}"
+print(formatted)
