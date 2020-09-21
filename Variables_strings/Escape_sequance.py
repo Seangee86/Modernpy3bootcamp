9c1c872667fc808800289e8669c46be689996878
Sean Granier <seanlg624@gmail.com>
2020-09-21 15:16:29 -0500

    escape sequance complete

---
+++ Variables_strings/Escape_sequance.py
@@ -0,0 +1,16 @@
+# Set the message variable equal to any string containing a new-line escape sequence
+message = "hello jimmy!\nhow are you doing?"
+
+
+# Add a string to the mountains variable that when printed results in: /\/\/\
+# You will need to use an escape sequence more than once!
+mountains = "/\\/\\/\\"
+
+
+# Set the quotation variable to any string that contains an escaped double quotation mark
+
+quotation = "he said \"hey yall\""
+
+print(message)
+print(mountains)
+print(quotation)
