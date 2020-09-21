43219e8768664cb10fa9b83ea14d044517a95fee
Sean Granier <seanlg624@gmail.com>
2020-09-21 14:44:39 -0500

    make some variables complete

---
+++ Variables_strings/Make_some_Variables.py
@@ -0,0 +1,10 @@
+# Define a variable named city and set it equal to any string
+city = "Oceanside"
+
+# Define a variable named price and set it equal to any float
+price = float(40)
+
+# Define a variable named high_score and set it equal to any int
+high_school = int('123')
+# Define a variable named is_having_fun and set it to a Boolean value
+is_having_fun = True
